5ac2cf927337ce26ecfe2ad56ff5efeeeaff7824
abrubel <abdur.techvill@gmail.com>
2023-01-22 23:31:04 +0600

    feat: Add word calculation functionality

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,22 @@ function App() {
   const [inputtedData, setInputedData] = useState("");
   let handleChange = event => {
     const {value} = event.target;
-    setInputedData(value)
+    setInputedData(value);
   }
+
+  let calculateWordCount = text => {
+    const wordsArr = text.trim().split(" ");
+    const filteredWords = wordsArr.filter(word => word !== "");
+    console.log(filteredWords.length);
+    return filteredWords.length;
+  }
+  
   return (
     <>
       <h1>How fast do you type?</h1>
       <textarea onChange={handleChange} value={inputtedData} />
       <h4>Time Remaining: ???</h4>
-      <button>Start</button>
+      <button onClick={() => calculateWordCount(inputtedData)}>Start</button>
       <h1>Word Count: ???</h1>
       <div>{inputtedData}</div>
     </>
